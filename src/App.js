e65850d2f4333c77cadd30cc8ed7b43026115c18
Anurag <anuragv8269@gmail.com>
2024-09-10 12:31:45 +0530

    components builds

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Celsius from "./celsius";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <Celsius />
+    </div>
+  );
+}
---
+++ src/celsius.js
@@ -0,0 +1,10 @@
+export default function Celsius(){
+
+    //creating states
+    return (
+        <div className="box">
+            <h2>Farenheit to Celsius</h2>
+            <input placeholder="Farenheit"></input>
+        </div>
+    )
+}
